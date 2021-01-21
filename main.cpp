b2e7bcdaa8ae15668fbf9c63695a70417105afea
Kristina Goncharova <codingkris@yahoo.com>
2021-01-21 22:33:01 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <uwebsockets/App.h>
+const std::string BROAD_CHANNEL = "broadcast";
 const std::string SET_NAME = "SET_NAME::";
 const std::string DIRECT = "DIRECT::";
+const std::string TOALL = "TOALL::";
+
+bool isToAllCommand(std::string_view message) {
+  return message.find(TOALL) == 0;
+}
+
+std::string parseToAllText(std::string_view message) {
+  return std::string(message.substr(TOALL.length()));
+}
 
 bool isSetNameCommand(std::string_view message) {
   return message.find(SET_NAME) == 0;
@@ -29,6 +39,14 @@ bool isDirectCommand(std::string_view message) {
   return message.find(DIRECT) == 0;
 }
 
+std::string makeOnline(int user_id, std::string user_name) {
+  return "ONLINE::" + std::to_string(user_id) + "::" + user_name;
+}
+
+std::string makeOffline(int user_id, std::string user_name) {
+  return "OFFLINE::" + std::to_string(user_id) + "::" + user_name;
+}
+
 int main() {
   struct PerSocketData {
     int user_id;
@@ -36,43 +54,56 @@ int main() {
   };
 
   int last_user_id = 10;
+  std::map<int, std::string> usersOnline;
 
   uWS::App()
     .ws<PerSocketData>("/*", {
-      /* Settings */
-      .compression = uWS::SHARED_COMPRESSOR,
-      .maxPayloadLength = 16 * 1024,
-      .idleTimeout = 600,
-      .maxBackpressure = 1 * 1024 * 1024,
+    /* Settings */
+    .compression = uWS::SHARED_COMPRESSOR,
+    .maxPayloadLength = 16 * 1024,
+    .idleTimeout = 600,
+    .maxBackpressure = 1 * 1024 * 1024,
 
-      /* Handlers */
-      .upgrade = nullptr,
-      .open = [&last_user_id](auto* connection) {
-        std::cout << "New connection created\n";
-        PerSocketData * userData = (PerSocketData*)connection->getUserData();
-        userData->user_id = last_user_id++;
-        userData->name = "UNNAMED";
-        connection->subscribe("broadcast");
-        connection->subscribe("user#" + std::to_string(userData->user_id));
-    },
-    .message = [](auto* connection, std::string_view message, uWS::OpCode opCode) {
-        std::cout << "New message recieved: \n" << message << '\n';
-        PerSocketData* userData = (PerSocketData*)connection->getUserData();
-        if (isSetNameCommand(message)) {
-          std::cout << "User set their name\n";
-          userData->name = parseName(message);
+    /* Handlers */
+    .upgrade = nullptr,
+    .open = [&last_user_id, &usersOnline](auto* connection) {
+      std::cout << "New connection created\n";
+      PerSocketData* userData = (PerSocketData*)connection->getUserData();
+      userData->user_id = last_user_id++;
+      userData->name = "UNNAMED";
+      usersOnline[userData->user_id] = userData->name;
+      connection->subscribe(BROAD_CHANNEL);
+      connection->subscribe("user#" + std::to_string(userData->user_id));
+  },
+  .message = [&usersOnline](auto* connection, std::string_view message, uWS::OpCode opCode) {
+      std::cout << "New message recieved: \n" << message << '\n';
+      PerSocketData* userData = (PerSocketData*)connection->getUserData();
+      if (isSetNameCommand(message)) {
+        std::cout << "User set their name\n";
+        userData->name = parseName(message);
+        usersOnline[userData->user_id] = userData->name;
+        connection->publish(BROAD_CHANNEL, makeOnline(userData->user_id, userData->name));
+        for (auto entry : usersOnline) {
+          connection->send(makeOnline(entry.first, entry.second), uWS::OpCode::TEXT);
         }
-        if (isDirectCommand(message)) {
-          std::cout << "User sent direct message\n";
-          std::string id = parseRecieverId(message);
-          std::string text = parseDirectMessage(message);
-          connection->publish("user#" + id, text);
-        }
+      }
+      if (isDirectCommand(message)) {
+        std::cout << "User sent direct message\n";
+        std::string id = parseRecieverId(message);
+        std::string text = parseDirectMessage(message);
+        connection->publish("user#" + id, "DIRECT::" + userData->name + "::" + text);
+      }
+      if (isToAllCommand(message)) {
+        std::string text = parseToAllText(message);
+        connection->publish(BROAD_CHANNEL, "TOALL::" + userData->name + "::" + text);
+      }
     },
-    .close = [](auto*/*ws*/, int /*code*/, std::string_view /*message*/) {
+    .close = [&usersOnline](auto* connection, int /*code*/, std::string_view /*message*/) {
       std::cout << "Connection closed\n";
-
-}
+      PerSocketData* userData = (PerSocketData*)connection->getUserData();
+      connection->publish(BROAD_CHANNEL, makeOffline(userData->user_id, userData->name));
+      usersOnline.erase(userData->user_id);
+    }
     }).listen(9001, [](auto* listen_socket) {
     if (listen_socket) {
       std::cout << "Listening on port " << 9001 << std::endl;
